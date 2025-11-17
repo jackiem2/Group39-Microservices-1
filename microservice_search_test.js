a40dfe2ae6345485c65459985e6a4a186bcf0a73
jackiem2 <jacqueline.merlan@gmail.com>
2025-11-16 21:22:35 -0600

    created request and JSON data

--- microservice_search_test.js
+++ microservice_search_test.js
@@ -13,3 +13,30 @@ const params = new URLSearchParams({
 
 // Builds the full request 
 const url = baseUrl + "?" + params.toString();
+
+// SEND the request using fetch() 
+fetch(url)
+  // The response comes back here
+  .then((response) => {
+    console.log("HTTP status:", response.status);
+
+    return response.json();
+  })
+
+  // JSON data 
+  .then((data) => {
+    console.log("\nFull JSON Response:");
+    console.log(JSON.stringify(data, null, 2));
+
+    console.log("\nItem Titles:");
+    data.items.forEach((item) => console.log("- " + item.title));
+
+    if (data.items.length === 0) {
+      console.log("No results found.");
+    }
+  })
+
+  // Prints error msg if request fails
+  .catch((error) => {
+    console.error("Error calling Search Microservice:", error);
+  });
